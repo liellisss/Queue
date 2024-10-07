c6e64617c82c4c944f6038ede592237010f5dab8
Elizaveta <lizalisovenkoYT@gmail.com>
2024-10-07 14:49:30 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace очередь
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить элемент";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(486, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удалить элемент";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(90, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Элемент в начале";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(442, 314);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(273, 52);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Количество элементов";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 60);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(0, 61);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Очередь";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace очередь
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> queue = new Queue<string>();
+        static Double value = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void print_queue(Queue<string> temp_queue)
+        {
+            label1.Text = "";
+            foreach (string element in queue)
+            {
+                label1.Text += element;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string result = Microsoft.VisualBasic.Interaction.InputBox("Введите элемент:");
+            queue.Enqueue(result);
+            print_queue(queue);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            queue.Dequeue();
+            print_queue(queue);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(queue.Peek(), "Первый элемент");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var que = queue.Count().ToString();
+            MessageBox.Show(que, "Количество элементов в очереди");
+        }
+    }
+}
